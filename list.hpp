91a553cf63b5193618237ecd5aa45d8bc528913a
kreftus <atunkin64@icloud.com>
2022-07-02 11:19:36 +0300

    add push and pop front/back

--- list.hpp
+++ list.hpp
@@ -41,30 +41,110 @@ void Node<T>::set_value(const T& new_Value){
 template<typename T>
 class list{
 public:
-    list(): m_Head(), m_Size(1) {};
-    list(unsigned int count, const T& value);
+    list(): m_Head(nullptr), m_Back(m_Head), m_Size(0){}
+    ~list() {free();}
 
-public:
-    T& front() {return m_Head->m_Value;}
+    T& front() {return m_Head->value();}
+    const T& front() const  {return m_Head->value();}
+
+    T& back() {return m_Back->value();}
+    const T& back() const {return m_Back->value();}
+
+    Node<T>* begin() {return m_Head;};
+    const Node<T>* begin() const {return m_Head;};
+
+    Node<T>* end() {return m_Back;}
+    const Node<T>* end() const {return m_Back;}
+    
+    unsigned int size() const {return m_Size;}
+    bool empty() const {return m_Size == 0;}
+
+    void clear() {free();}
+
+    void push_back(const T& new_Value);
+    void push_front(const T& new_Value);
+
+    void pop_back();
+    void pop_front();
+
 private:
+    void free();
+
     std::allocator<Node<T>> m_Allocator;
+    const unsigned int m_Single = 1;
     Node<T>* m_Head;
     Node<T>* m_Back;
     unsigned int m_Size;
 };
 
 template<typename T>
-list<T>::list(unsigned int count, const T& value): m_Head(value), m_Size(count){
+void list<T>::free(){
     Node<T>* current = m_Head;
-    for (unsigned int i = 1; i < count; i++)
+    for(unsigned int i = 0; i < m_Size; i++)
     {
-        Node<T>* next = new Node<T>(value);
-        current.set_next(next);
-        current = next;
-        if (i == count - 1)
-        {
-            m_Back
-        }
-        
+        m_Head = m_Head->next();
+        m_Allocator.destroy(current);
+        m_Allocator.deallocate(current, 1);
+        current = m_Head;  
     }
+    m_Size = 0;
+}
+
+template<typename T>
+void list<T>::push_back(const T& new_Value){
+    Node<T>* next = m_Allocator.allocate(1);
+    m_Allocator.construct(next, new_Value);
+
+    if (m_Size > 1) m_Back->set_next(next);
+    else if(m_Size == 1) m_Head->set_next(next);
+    else m_Head = next;
+    
+    m_Back = next;
+    m_Size++;
+}
+
+template<typename T>
+void list<T>::push_front(const T& new_Value){
+    Node<T>* next = m_Allocator.allocate(1);
+    m_Allocator.construct(next, new_Value);
+
+    if (m_Size > 0) next->set_next(m_Head);
+    else m_Back = next;
+    m_Head = next;
+    m_Size++;
+}
+
+template<typename T>
+void list<T>::pop_back(){
+    if(this->empty()) return;
+
+    Node<T>* current = m_Head;
+
+    if(m_Size != 1) 
+        while(current->next() != m_Back) current = current->next();
+    current->set_next(nullptr);
+    m_Allocator.destroy(m_Back);
+    m_Allocator.deallocate(m_Back, 1);
+    m_Back = current;
+    // if (m_Size == 1) {
+    //     m_Head = nullptr;
+    //     m_Back = nullptr;
+    // }
+    m_Size--;
+}
+
+template<typename T>
+void list<T>::pop_front(){
+    if(this->empty()) return;
+
+    Node<T>* current = m_Head;
+    
+    m_Head = m_Head->next();
+    m_Allocator.destroy(current);
+    m_Allocator.deallocate(current, 1);
+    // if (m_Size == 1) {
+    //     m_Head = nullptr;
+    //     m_Back = nullptr;
+    // }
+    m_Size--;
 }
