518f4b15c24b5dbd1a19f7bc31b28395d8645f8c
kreftus <atunkin64@icloud.com>
2022-07-02 15:45:59 +0300

    add implementation by list

--- stack.hpp
+++ stack.hpp
@@ -1,18 +1,36 @@
 #include "vector.hpp"
+#include "list.hpp"
 
 template<typename T>
-class stack{
+class astack{
 public:
-    stack() = default;
+    astack() = default;
     
     T& top() {return m_Buffer.back();}
     const T& top() const {return m_Buffer.back();}
 
     unsigned int size() const {return m_Buffer.size();}
-    bool empty() const {return m_Buffer.size() == 0;}
+    bool empty() const {return m_Buffer.empty();}
 
     void push(const T& new_value){m_Buffer.push_back(new_value);}
     void pop() {m_Buffer.pop_back();}
 private:
     vector<T> m_Buffer;
+};
+
+template<typename T>
+class lstack{
+public:
+    lstack() = default;
+    
+    T& top() {return m_Buffer.front();}
+    const T& top() const {return m_Buffer.front();}
+
+    unsigned int size() const {return m_Buffer.size();}
+    bool empty() const {return m_Buffer.empty();}
+
+    void push(const T& new_value){m_Buffer.push_front(new_value);}
+    void pop() {m_Buffer.pop_front();}
+private:
+    list<T> m_Buffer;
 };
