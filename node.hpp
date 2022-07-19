bc2dd74f07a61566d39c1d16287072bc576e48b3
a-int <atunkin64@icloud.com>
2022-07-19 12:01:18 +0300

    change avaibale constructors

--- node.hpp
+++ node.hpp
@@ -4,35 +4,35 @@
 template<typename T>
 class Node{
 public:
-    Node(): m_Value(m_Allocator.allocate(m_Size)), m_Next(nullptr), m_Previous(nullptr) {m_Allocator.construct(m_Value);}
-    Node(const T& val): m_Value(m_Allocator.allocate(m_Size)), m_Next(nullptr), m_Previous(nullptr) {m_Allocator.construct(m_Value, val);}
-    Node(const T& val, const Node<T>& Next): m_Value(m_Allocator.allocate(m_Size)), m_Next(&Next), m_Previous(nullptr) {m_Allocator.construct(m_Value, val);}
-    Node(const T& val, const Node<T>& Next, const Node<T>& Prev): m_Value(m_Allocator.allocate(m_Size)), m_Next(&Next), m_Previous(&Prev) {m_Allocator.construct(m_Value, val);}
+    Node(const T& val                                                   ): m_Value(m_Allocator.allocate(m_Size)), m_Next(nullptr), m_Previous(nullptr) {m_Allocator.construct(m_Value, val);}
+    Node(               Node<T>* next = nullptr, Node<T>* prev = nullptr): m_Value(nullptr), m_Next(next), m_Previous(prev){}
+    Node(const T& val,  Node<T>* next = nullptr, Node<T>* prev = nullptr): m_Value(m_Allocator.allocate(1)), m_Next(next), m_Previous(prev){m_Allocator.construct(m_Value, val);}
     ~Node(){free();}
+
 private:
     void free();
 
 public:
-    Node<T>* next() {return m_Next;}
-    const Node<T>* next() const {return m_Next;}
+    Node<T>*        next()              {return m_Next;}
+    const Node<T>*  next()      const   {return m_Next;}
     
-    Node<T>* previous() {return m_Previous;}
-    const Node<T>* previous() const {return m_Previous;}
+    Node<T>*        previous()          {return m_Previous;}
+    const Node<T>*  previous()  const   {return m_Previous;}
     
-    T& value() {return *m_Value;}
-    const T& value() const {return *m_Value;}
+    T&              value()             {return *m_Value;}
+    const T&        value()     const   {return *m_Value;}
     
     
-    void set_next(Node<T>* Node) {m_Next = Node;}
-    void set_previous(Node<T>* Node) {m_Previous = Node;}
+    void set_next(Node<T>* Node)        {m_Next = Node;}
+    void set_previous(Node<T>* Node)    {m_Previous = Node;}
     void set_value(const T& new_Value);
 
 private:
-    std::allocator<T> m_Allocator;
-    const unsigned int m_Size = 1;
-    T* m_Value;
-    Node<T>* m_Next;
-    Node<T>* m_Previous;
+    std::allocator<T>   m_Allocator;
+    const unsigned int  m_Size = 1;
+    T*                  m_Value;
+    Node<T>*            m_Next;
+    Node<T>*            m_Previous;
 };
 
 template<typename T>
